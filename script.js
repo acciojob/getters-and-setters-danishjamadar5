e2425cb507eeae9fadcca43017689cff25bb1aae
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-06-17 12:17:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,13 +15,18 @@ class Person {
 
 class Student extends Person {
   study() {
-    console.log(`${this.name} is studying.`);
+    console.log(`${this._name} is studying`);
   }
 }
 
 class Teacher extends Person {
   teach() {
-    console.log(`${this.name} is teaching.`);
+    console.log(`${this._name} is teaching`);
   }
 }
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 
